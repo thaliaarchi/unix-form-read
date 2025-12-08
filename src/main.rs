c5ddcbd298857ba8c34c950cc7421ffd2659a167
Thalia Archibald <thalia@archibald.dev>
2025-12-08 11:54:04 -0700

    Dump occurrences of lines in form.m

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+    io::{Write, stdout},
+};
+
+use serde::Serialize;
+
+fn main() {
+    let form = fs::read("distr/form.m").unwrap();
+    let text = fs::read_to_string("distr/y").unwrap();
+    assert!(text.chars().all(|c| c.is_ascii()));
+
+    #[derive(Default, Serialize)]
+    struct Entry<'a> {
+        text: &'a str,
+        lines: Vec<Line<'a>>,
+        offsets: Vec<usize>,
+    }
+    #[derive(Serialize)]
+    struct Line<'a> {
+        name: &'a str,
+        line: usize,
+    }
+
+    let mut line_map = HashMap::<&str, Entry>::new();
+    let mut names = HashSet::new();
+    let mut name_number = None;
+
+    for (line_number, line) in text.split("\n").enumerate() {
+        if line.is_empty() {
+            continue;
+        }
+        if let [b'n', b'a', b'm', b'e', b'-', .., b':'] = line.as_bytes() {
+            let name = &line[5..line.len() - 1];
+            assert!(names.insert(name), "non-unique name");
+            name_number = Some(name);
+            continue;
+        }
+        let entry = line_map.entry(line).or_default();
+        let name_number = name_number.expect("missing name number before first line");
+        entry.text = line;
+        entry.lines.push(Line {
+            name: name_number,
+            line: line_number,
+        });
+    }
+
+    for (&line, entry) in &mut line_map {
+        let matches = form
+            .windows(line.len())
+            .enumerate()
+            .filter_map(|(offset, window)| (window == line.as_bytes()).then_some(offset));
+        entry.offsets.extend(matches);
+    }
+
+    let mut entries = line_map.into_values().collect::<Vec<_>>();
+    entries.sort_by_key(|entry| entry.lines[0].line);
+    let mut out = stdout().lock();
+    for entry in &entries {
+        serde_json::to_writer(&mut out, entry).unwrap();
+        out.write_all(b"\n").unwrap();
+    }
+}
