07226a2eb29713d66025c7b0478174df4eb381ff
Thalia Archibald <thalia@archibald.dev>
2025-12-08 18:52:10 -0700

    Decode other header fields

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::{collections::HashSet, fs, mem};
+use std::{
+    collections::HashSet,
+    fs,
+    mem::{self, offset_of},
+};
 
 use bstr::BStr;
 
-/// A four-word header for a block. Source: V5 form6.s.
+/// A four-word header for a block (V5 form6.s).
 #[derive(Debug)]
 #[repr(C)]
 struct Header {
@@ -16,20 +20,46 @@ struct Header {
     end: u16,
 }
 
+/// Header data dumped from V5 form6.s:hblk..headend in `.bss`.
+#[repr(C)]
+struct Headers {
+    /// Pointer to free header (V5 form6.s:hblk).
+    free_header: u16,
+    /// A list of pointers to free headers? (V5 form6.s:frlist).
+    free_list: [u16; 16],
+    /// ? (V5 form6.s:asmdisc).
+    asmdisc: u16,
+    /// The header blocks (V5 form6.s:headers).
+    headers: [Header; 763],
+    pad: [u16; 2],
+}
+
+/// The size of the headers area (V5 form6.s:hsz).
+const HEADERS_SIZE: u16 = 6144;
+/// The size of the data area (V5 form6.s:datasz).
+const DATA_SIZE: u16 = 32768;
+
+const _: () = assert!(size_of::<Headers>() == HEADERS_SIZE as usize);
+
 fn main() {
     let form = fs::read("distr/form.m").unwrap();
 
     let strings = fs::read_to_string("strings.json").unwrap();
     let strings: Vec<String> = serde_json::from_str(&strings).unwrap();
 
-    // See V5 form6.s `hsz=6144.`
-    assert!(form.len() >= 6144);
-    let mut i = 4;
-    while i + 8 < 6144 {
-        let header: [u8; 8] = form[i..i + 8].try_into().unwrap();
-        let header: Header = unsafe { mem::transmute(header) };
-        println!("{i}: {header:?}");
-        if header.start <= header.end
+    let headers: &[u8; HEADERS_SIZE as _] = form.first_chunk().unwrap();
+    let headers: Headers = unsafe { mem::transmute(*headers) };
+    println!("free_header: {:?}", headers.free_header);
+    println!("free_list: {:?}", headers.free_list);
+    println!("asmdisc: {:?}", headers.asmdisc);
+    for (i, header) in headers.headers.iter().enumerate() {
+        let offset = offset_of!(Headers, headers) + size_of::<Header>() * i;
+        println!("{offset}: {header:?}");
+        // The first two conditions are from V5 form6.s:preposterous;
+        // the others are inferred.
+        if header.start >= HEADERS_SIZE
+            && header.end <= HEADERS_SIZE + DATA_SIZE
+            && header.start <= header.end
             && (header.end as usize) <= form.len()
             && ((header.start..=header.end).contains(&header.read) || header.read == 0)
             && ((header.start..=header.end).contains(&header.write) || header.write == 0)
@@ -49,8 +79,8 @@ fn main() {
         } else {
             println!("  Invalid!");
         }
-        i += 8;
     }
+    println!("pad: {:?}", headers.pad);
 
     let mut labels = Vec::new();
     let mut offsets = HashSet::new();
